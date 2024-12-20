a0d88f2f52e3477c8e97b8215a106ccd649a4512
zxxsong <zxxsong@gmail.com>
2024-12-20 18:58:53 +0800

    Add pretrained model

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,9 +1,8 @@
 # Copyright 2022 CircuitNet. All rights reserved.
 
 from .congestion_dataset import CongestionDataset
-from .drc_dataset import DRCDataset
-from .irdrop_dataset import IRDropDataset
+
 from .superblue_dataset import SuperBlueDataset
 
-__all__ = ['CongestionDataset', 'DRCDataset', 'IRDropDataset', 'SuperBlueDataset']
+__all__ = ['CongestionDataset', 'SuperBlueDataset']
 
